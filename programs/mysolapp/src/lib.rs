3882a5c1046a8f924d5e424d2bc97f1e321bb0be
dannyvi <dannyvis@icloud.com>
2022-07-22 09:21:49 +0800

    Initial commit

---
+++ programs/mysolapp/src/lib.rs
@@ -0,0 +1,41 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6tqS5yifeM9MH9wpFC2btFXBYfTgR25YPpTegdXftAJe");
+
+#[program]
+pub mod mysolapp {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+    use super::*;
+
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
